210c32426b91e57aead1f3a9d274f94df2135060
Daniil <Soboliev.danik@gmail.com>
2024-03-28 21:39:33 +0200

    First hw

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/*
+Компонент представляет собой класс AppComponent, перед которым указана функция
+декоратор @Component.
+*/
+@Component({
+  selector: 'app-root',  // название селектора для отображения во view
+  templateUrl: './app.component.html',  // представление с вкраплением кода Angular
+  styleUrl: './app.component.css'  // набор стилей
+})
+export class AppComponent {
+  title = 'FirstProject';
+}
---
+++ app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+/*
+Декоратор NgModule() — это функция, принимает один объект метаданных, свойства которого описывают модуль.
+Свойство declarations - передает Angular список директив, компонентов и каналов (pipes), которые принадлежат этому модулю.
+Свойство imports - для перечисления других модулей, чьи экспортированные классы нужны в этом модуле.
+Свойство providers - определяет сервисы, которые хотите добавить в глобальную коллекцию сервисов.
+  Эти сервисы будут доступны через внедрение зависимостей (dependency injection).
+Свойство bootstrap - указывает основной компонент этого модуля, который должен быть загружен при загрузке модуля.
+
+Корневой модуль приложения импортирует модуль BrowserModule, который предоставляет функциональность, необходимую
+для запуска приложений Angular в браузерах.
+Так же импортируется компонент AppComponent и объявляется как основной в свойстве bootstrap.
+*/
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
